2dd9061b4dafd1f2fa25cc30a1fa07c6ee8b97af
Henrico <henricozondag@gmail.com>
2016-03-08 22:49:12 +0100

    Hoppakee

---
+++ app/src/main/java/mamahetogames/eliterisk/HighScores.java
@@ -0,0 +1,14 @@
+package mamahetogames.eliterisk;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighScores extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_scores);
+    }
+}
--- app/src/main/java/mamahetogames/eliterisk/Menu.java
+++ app/src/main/java/mamahetogames/eliterisk/Menu.java
@@ -1,16 +1,36 @@
 package mamahetogames.eliterisk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
-public class Menu extends Activity {
+public class Menu extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
+        Button buttonHighScores = (Button) findViewById(R.id.buttonHighScores);
+        buttonHighScores.setOnClickListener(this);
+        Button buttonSettings = (Button) findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.buttonHighScores:
+                i = new Intent(this, HighScores.class);
+                startActivity(i);
+                break;
+            case R.id.buttonSettings:
+                i = new Intent(this, Settings.class);
+                startActivity(i);
+                break;
+        }
+    }
 
 }
